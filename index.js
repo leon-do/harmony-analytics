e4a92179a47bf93357b763d64ed9150f54b8e16a
leon-do <leondo123@gmail.com>
2022-10-11 17:24:11 -0400

    uncomment

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ async function main() {
   d.setDate(d.getDate() - 1);
   const date = d.toISOString().split("T")[0];
 
-  // const transactionCount = await fetch(`http://api1.explorer.t.hmny.io:3000/v0/metrics?type=transactions_count&offset=0&limit=180`, {
-  //   headers: {
-  //     rest_api_key: "harmony1",
-  //   },
-  // }).then((response) => response.json());
+  const transactionCount = await fetch(`http://api1.explorer.t.hmny.io:3000/v0/metrics?type=transactions_count&offset=0&limit=180`, {
+    headers: {
+      rest_api_key: "harmony1",
+    },
+  }).then((response) => response.json());
   // console.log({ transactionCount });
 
-  // const walletCount = await fetch(`http://api1.explorer.t.hmny.io:3000/v0/metrics?type=wallets_count&offset=0&limit=180`, {
-  //   headers: {
-  //     rest_api_key: "harmony1",
-  //   },
-  // }).then((response) => response.json());
+  const walletCount = await fetch(`http://api1.explorer.t.hmny.io:3000/v0/metrics?type=wallets_count&offset=0&limit=180`, {
+    headers: {
+      rest_api_key: "harmony1",
+    },
+  }).then((response) => response.json());
   // console.log({ walletCount });
 
-  // const averageFee = await fetch(`http://api1.explorer.t.hmny.io:3000/v0/metrics?type=average_fee&limit=180`, {
-  //   headers: {
-  //     rest_api_key: "harmony1",
-  //   },
-  // }).then((response) => response.json());
+  const averageFee = await fetch(`http://api1.explorer.t.hmny.io:3000/v0/metrics?type=average_fee&limit=180`, {
+    headers: {
+      rest_api_key: "harmony1",
+    },
+  }).then((response) => response.json());
   // console.log({ averageFee });
 
   const blockSize = await fetch(`http://api1.explorer.t.hmny.io:3000/v0/metrics?type=block_size&limit=180`, {
@@ -32,10 +32,11 @@ async function main() {
       rest_api_key: "harmony1",
     },
   }).then((response) => response.json());
+  // console.log({ blockSize });
 
-  for (let i = 0; i < blockSize.length; i++) {
-    const date = blockSize[i].date.split("T")[0];
-    const value = blockSize[i].value;
+  for (let i = 0; i < averageFee.length; i++) {
+    const date = averageFee[i].date.split("T")[0];
+    const value = averageFee[i].value;
     console.log(date, value);
   }
 }
